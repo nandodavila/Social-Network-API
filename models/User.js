c11db46f86c562aab32b7418919e79340f1d264c
Nando <nandodavila94@gmail.com>
2022-01-04 19:04:26 -0500

    user model done

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema]
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  })
+  
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
