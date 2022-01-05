a91fc8dde22f8539b7c741f957df40145d750f50
Nando <nandodavila94@gmail.com>
2022-01-04 19:14:09 -0500

    mostly done thoughts model, but kinda confused at reaction schema/model

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    thoughtName: {
+      type: String,
+      required: true,
+      maxlength: 120,
+      minlength: 1,
+      default: 'Unnamed thought',
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = thoughtSchema;
